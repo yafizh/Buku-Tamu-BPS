c72eaf515ef10806faeac65737d46b78b6585970
yafizh <a.chairil.yafizh@gmail.com>
2022-01-25 01:58:57 +0800

    set data divisi and pegawai

--- database/database.sql
+++ database/database.sql
@@ -22,7 +22,13 @@ INSERT INTO `tabel_divisi` (
     nama_divisi,
     keterangan
 ) VALUES 
-('Divisi1', 'Contoh1');
+('KEPALA', 'Divisi Bagian KEPALA'),
+('TATA USAHA', 'Divisi Bagian TATA USAHA'),
+('SEKSI STATISTIK SOSIAL', 'Divisi Bagian SEKSI STATISTIK SOSIAL'),
+('SEKSI NERWILIS', 'Divisi Bagian SEKSI NERWILIS'),
+('SEKSI STATISTIK DISTRIBUSI', 'Divisi Bagian SEKSI STATISTIK DISTRIBUSI'),
+('SEKSI STATISTIK PRODUKSI', 'Divisi Bagian SEKSI STATISTIK PRODUKSI'),
+('SEKSI IPDS', 'Divisi Bagian SEKSI IPDS');
 
 CREATE TABLE `tabel_pegawai` (
     id INT NOT NULL AUTO_INCREMENT,
@@ -42,26 +48,26 @@ INSERT INTO `tabel_pegawai` (
     jenis_kelamin,
     nomor_telepon
 ) VALUES 
-(1,'Sukma Handayani, M.Si', '197503111996122000', 'P', '0'),
-(1,'Arfiani, S.Si', '198112042011011000', 'L', '0'),
-(1,'M. Agus Suharyanto, A.Md', '198810052011011000', 'L', '0'),
-(1,'Noor Hamimah, SE', '198607212011012000', 'P', '0'),
-(1,'Muhammad Taufik Hidayat, SST', '198803082012111000', 'L', '0'),
-(1,'Hanif Yontar Rahma, S.Si', '199412192019032000', 'L', '0'),
-(1,'Muhammad Adi Wijaya Kesuma, SST', '199412212018021000', 'L', '0'),
-(1,'Masdani, SE', '197110181993121000', 'L', '0'),
-(1,'Ulya Zahrotun Niswah, SST', '199407242018022000', 'P', '0'),
-(1,'Anggita Silmi Nabilah, SST', '199507062018022000', 'P', '0'),
-(1,'Muhammad Imam Sholihin, S.Tr.Stat.', '199711142021041000', 'L', '0'),
-(1,'Eko Wahyu Lestari, SST', '199403272017012000', 'L', '0'),
-(1,'Oktaviani, S.Tr.Stat.', '199610122019012000', 'P', '0'),
-(1,'Rusni', '196905102006041000', 'L', '0'),
-(1,'Taufik Ilhakim', '196807052007011000', 'L', '0'),
-(1,'Abu Hurairah', '198108232002121000', 'P', '0'),
-(1,'Azehar, SE.', '196902091993121000', 'P', '0'),
-(1,'Agusriadi', '197708062007101000', 'L', '0'),
-(1,'Nabhani', '198010172007101000', 'P', '0'),
-(1,'Herliani', '197504112009012000', 'P', '0');
+(1,'Sukma Handayani, M.Si', '197503111996122000', 'P', '098732135523'),
+(2,'Arfiani, S.Si', '198112042011011000', 'L', '098732135524'),
+(3,'M. Agus Suharyanto, A.Md', '198810052011011000', 'L', '098732135522'),
+(4,'Noor Hamimah, SE', '198607212011012000', 'P', '098732135521'),
+(5,'Muhammad Taufik Hidayat, SST', '198803082012111000', 'L', '098732135527'),
+(6,'Hanif Yontar Rahma, S.Si', '199412192019032000', 'L', '098732135529'),
+(7,'Muhammad Adi Wijaya Kesuma, SST', '199412212018021000', 'L', '098732135528'),
+(1,'Masdani, SE', '197110181993121000', 'L', '098732135533'),
+(1,'Ulya Zahrotun Niswah, SST', '199407242018022000', 'P', '098732135534'),
+(2,'Anggita Silmi Nabilah, SST', '199507062018022000', 'P', '098732135531'),
+(3,'Muhammad Imam Sholihin, S.Tr.Stat.', '199711142021041000', 'L', '098732135532'),
+(4,'Eko Wahyu Lestari, SST', '199403272017012000', 'L', '098732135535'),
+(5,'Oktaviani, S.Tr.Stat.', '199610122019012000', 'P', '098732135536'),
+(6,'Rusni', '196905102006041000', 'L', '098732135537'),
+(7,'Taufik Ilhakim', '196807052007011000', 'L', '098732135538'),
+(1,'Abu Hurairah', '198108232002121000', 'P', '098732135539'),
+(2,'Azehar, SE.', '196902091993121000', 'P', '098732135599'),
+(3,'Agusriadi', '197708062007101000', 'L', '098732135588'),
+(4,'Nabhani', '198010172007101000', 'P', '098732135566'),
+(5,'Herliani', '197504112009012000', 'P', '098732135511');
 
 CREATE TABLE `tabel_tamu` (
     id INT NOT NULL AUTO_INCREMENT,
@@ -131,3 +137,4 @@ AS
         (SELECT COUNT(id) FROM tabel_tamu WHERE tabel_tamu.id_pegawai=tabel_pegawai.id) AS jumlah_pengunjung 
     FROM 
         tabel_pegawai;
+        
