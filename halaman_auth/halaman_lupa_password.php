ac64af52ec80bfb490d47c9ac02e9d4e71ab40a9
yafizh <a.chairil.yafizh@gmail.com>
2022-02-06 18:17:42 +0800

    change title

--- halaman_auth/halaman_lupa_password.php
+++ halaman_auth/halaman_lupa_password.php
@@ -30,7 +30,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Buku Tamu BPS</title>
     <link rel="shortcut icon" href="../assets/images/favicon.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
