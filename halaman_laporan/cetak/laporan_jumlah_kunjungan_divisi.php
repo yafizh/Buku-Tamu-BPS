5c9f058904b6d7b318414fef7e615bdd763e9140
yafizh <a.chairil.yafizh@gmail.com>
2022-02-06 18:13:22 +0800

    change city on every report

--- halaman_laporan/cetak/laporan_jumlah_kunjungan_divisi.php
+++ halaman_laporan/cetak/laporan_jumlah_kunjungan_divisi.php
@@ -97,7 +97,7 @@ const BULAN_DALAM_INDONESIA = [
         </table>
         <div style="display: flex; justify-content: end;">
             <div style="text-align: center; margin-top: 20px; padding: 10px; width: 200px;">
-                <span>Martapura, <?= Date('d') ?> <?= BULAN_DALAM_INDONESIA[Date('m') - 1] ?> <?= Date('Y') ?></span>
+                <span>Amuntai, <?= Date('d') ?> <?= BULAN_DALAM_INDONESIA[Date('m') - 1] ?> <?= Date('Y') ?></span>
                 <br>
                 <span>Mengetahui</span>
                 <br><br><br><br><br>
--- halaman_laporan/cetak/laporan_tamu_per_tahun.php
+++ halaman_laporan/cetak/laporan_tamu_per_tahun.php
@@ -104,7 +104,7 @@ $id_pegawai = $_POST['id_pegawai'];
         </table>
         <div style="display: flex; justify-content: end;">
             <div style="text-align: center; margin-top: 20px; padding: 10px; width: 200px;">
-                <span>Martapura, <?= Date('d') ?> <?= $BULAN_DALAM_INDONESIA[Date('m') - 1] ?> <?= Date('Y') ?></span>
+                <span>Amuntai, <?= Date('d') ?> <?= $BULAN_DALAM_INDONESIA[Date('m') - 1] ?> <?= Date('Y') ?></span>
                 <br>
                 <span>Mengetahui</span>
                 <br><br><br>
