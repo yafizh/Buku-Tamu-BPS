d1e778d78312a45920f540cca9d001016d9a9903
yafizh <a.chairil.yafizh@gmail.com>
2022-07-28 01:36:36 +0800

    sad

--- halaman_tambah_data/halaman_tambah_divisi.php
+++ halaman_tambah_data/halaman_tambah_divisi.php
@@ -21,28 +21,20 @@ if (isset($_POST['submit'])) {
 <div class="main-panel">
     <div class="content-wrapper">
         <div class="page-header">
-            <h3 class="page-title"> Tambah Divisi </h3>
-            <!-- <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Forms</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Form elements</li>
-                </ol>
-            </nav> -->
+            <h3 class="page-title text-dark"> Tambah Divisi </h3>
         </div>
         <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
-                <div class="card">
+                <div class="card bg-light">
                     <div class="card-body">
-                        <!-- <h4 class="card-title">Indentitas Tamu</h4> -->
-                        <!-- <p class="card-description"> Basic form layout </p> -->
                         <form class="forms-sample" action="" method="POST">
                             <div class="form-group">
-                                <label for="nama_divisi">Nama Divisi</label>
-                                <input type="text" class="form-control text-white" name="nama_divisi" autocomplete="off" placeholder="Masukkan Nama Divisi..." required>
+                                <label class="text-dark" for="nama_divisi">Nama Divisi</label>
+                                <input type="text" class="form-control text-dark bg-light" name="nama_divisi" autocomplete="off" placeholder="Masukkan Nama Divisi..." required>
                             </div>
                             <div class="form-group">
-                                <label for="keterangan">Keterangan</label>
-                                <input type="text" class="form-control text-white" name="keterangan" autocomplete="off" placeholder="Masukkan Keterangan..." required>
+                                <label class="text-dark" for="keterangan">Keterangan</label>
+                                <input type="text" class="form-control text-dark bg-light" name="keterangan" autocomplete="off" placeholder="Masukkan Keterangan..." required>
                             </div>
                             <div class="form-group">
                                 <button class="btn btn-dark" type="reset">Reset</button>
